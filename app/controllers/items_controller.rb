c09907f979b78c0d829d4df944d6bda94dd217ac
Jennifer <palacios.jennifer@gmail.com>
2015-03-17 19:26:25 +0100

    created delete action for items and halfway through creating edit action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,34 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+  @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if 
+      @item.update_attributes(params.require(:item).permit(:name))
+      flash[:notice] = "Item was updated"
+    else
+      flash[:error] = "Ooops, there was an error. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+  @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Item was deleted"
+      redirect_to current_user
+    else
+      flas[:error] = "Ooops, there was an error. Please try again."
+      render :show
+    end
+  end
 end
 
 
