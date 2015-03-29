9992513ff58e37a67d43f82398e6cd7c42c7a419
Jennifer <palacios.jennifer@gmail.com>
2015-03-29 16:31:42 +0200

    modified rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
-every 1.day, :at => '4:25pm' do 
+set :environment, "development"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+
+every 1.day, :at => '4:40pm' do 
   rake "todo:delete_items"
 end
