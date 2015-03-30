1eb65b35dedfcf552f73ad4b805cc863fff1043d
Jennifer <palacios.jennifer@gmail.com>
2015-03-30 18:48:35 +0200

    reverted changes to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,3 @@
-set :environment, "development"
-set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
-
-every 1.day, :at => '4:40pm' do 
+every 1.day, :at => '11:59pm' do 
   rake "todo:delete_items"
 end
